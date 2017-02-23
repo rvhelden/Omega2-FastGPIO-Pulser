e0996991884bb45cd51e2f83547ff3ec6d565b13
Ronald van Helden <r.vanhelden@bitstack.nl>
2017-02-23 22:00:39 +0100

    Some more cleanup

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #define HIGH 1
 #define LOW 0
 
-int pulseGpio(FastGpioOmega2 *gpio, int pin, int repeat, int pause);
+int start(int pin, int repeat, int pause);
 void pulse(FastGpioOmega2 *gpioObj, int pin, int pause);
 
 int main(int argc, char **argv)
@@ -11,14 +11,14 @@ int main(int argc, char **argv)
     int repeat = 15;
     printf("Pulsing for the %d times\n", repeat);
 
-    FastGpioOmega2 *gpio = new FastGpioOmega2();
-    pulseGpio(gpio, 1, repeat, 100000);
+    start(1, repeat, 100000);
 
     printf("Finished pulsing %d times\n", repeat);
 }
 
-int pulseGpio(FastGpioOmega2 *gpio, int pin, int repeat, int pause)
+int start(int pin, int repeat, int pause)
 {
+    FastGpioOmega2 *gpio = new FastGpioOmega2();
 	gpio->SetDirection(pin, 1);
 
     for (int i = 0; i < repeat; i++)
