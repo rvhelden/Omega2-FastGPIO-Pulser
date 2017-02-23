57363369e4e42a239e48fc9ce6eeaed3377895b6
Ronald van Helden <r.vanhelden@bitstack.nl>
2017-02-23 21:05:24 +0100

    Tidy up

--- main.cpp
+++ main.cpp
@@ -1,32 +1,37 @@
-#include "main.h"
 #include <fastgpioomega2.h>
 
-void pulse(FastGpioOmega2 *gpioObj, int pinNum, int highMicros, int lowMicros)
-{
-    gpioObj->Set(pinNum, 1);
-    usleep(highMicros);
-    gpioObj->Set(pinNum, 0);
-    usleep(lowMicros);
-}
+#define HIGH 1
+#define LOW 0
 
-int pulseGpio(FastGpioOmega2 *gpioObj, int pinNum, int repeats, int upTime, int downTime)
-{
-	gpioObj->SetDirection(pinNum, 1);
-
-    for (int i = 0; i < repeats; i++)
-    {
-        printf("Pulsed for the %dth time\n", i + 1);
-        pulse(gpioObj, pinNum, upTime, downTime);
-    }
-}
+int pulseGpio(FastGpioOmega2 *gpio, int pin, int repeat, int pause);
+void pulse(FastGpioOmega2 *gpioObj, int pin, int pause);
 
 int main(int argc, char **argv)
 {
-    int times = 15;
-    printf("Pulsing for the %d times\n", times);
+    int repeat = 15;
+    printf("Pulsing for the %d times\n", repeat);
 
-    FastGpioOmega2 *fast = new FastGpioOmega2();
-    pulseGpio(fast, 1, times, 50000, 50000);
+    FastGpioOmega2 *gpio = new FastGpioOmega2();
+    pulseGpio(gpio, 1, repeat, 100000);
 
-    printf("Finished pulsing %d times\n", times);
+    printf("Finished pulsing %d times\n", repeat);
+}
+
+int pulseGpio(FastGpioOmega2 *gpio, int pin, int repeat, int pause)
+{
+	gpio->SetDirection(pin, 1);
+
+    for (int i = 0; i < repeat; i++)
+    {
+        printf("Pulsed for the %dth time\n", i + 1);
+        pulse(gpio, pin, pause);
+    }
+}
+
+void pulse(FastGpioOmega2 *gpioObj, int pin, int pause)
+{
+    gpioObj->Set(pin, HIGH);
+    usleep(pause);
+    gpioObj->Set(pin, LOW);
+    usleep(pause);
 }
